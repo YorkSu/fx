4caca2dd99c4a3d38efded7942f84c1bbcd42325
YorkSu <york_su@qq.com>
2020-09-04 21:08:06 +0800

    2020-09-04 - 1

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""FX
+======
+
+useful python scripts
+"""
+
+
+__author__ = "York Su"
+__version__ = "0.1.0"
+
---
+++ __main__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""__main__
+
+The program entrance.
+"""
+
